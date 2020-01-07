208d7863641e4f1fafd38a1462a55fba3d1cbc67
qiangzhang <qiang.zhang@garena.cn>
2020-01-07 16:16:10 +0800

    a

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,14 @@ cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 echo 'www.easyrentsh.xyz' > CNAME
-
+echo '
+node_modules 
+.VSCODE
+docs/.vuepress/dist' > .gitignore
 git init
 git add -A
 git commit -m 'deploy'
+
 
 # 建立远程链接
 # git remote add origin/master https://github.com/harryzq/harryzq.github.io.git
